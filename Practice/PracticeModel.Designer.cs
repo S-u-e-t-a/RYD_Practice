189c63b0f7403659d7667349554a23d99955ad7e
Danya <ermakov1709@mail.ru>
2021-07-17 18:45:01 +0300

    Добавил единицы измерений во вспомогательных формах

--- Practice/PracticeModel.Designer.cs
+++ Practice/PracticeModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\aspir\source\repos\Practice\Practice\PracticeModel.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\Даня\source\repos\RYD_Practice\Practice\PracticeModel.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
